390839e8518767cc9134e48c89d5cd647f1a051b
Daphney <daphneyalexandre@hotmail.com>
2024-05-16 15:43:04 -0400

    complete

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
+let num1 = 31
+let num2 = 2
+
+const multiply = num1 * num2 
+
+const random = Math.floor(Math.random() * 10) + 1;
+    
+let num3 = 9
+let num4 = 5
+const mod =num3 % num4
+
+const max = Math.max(5, 10, 15, 20);
+
+
+
 // Write your code here
